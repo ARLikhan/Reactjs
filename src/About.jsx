cd426388178c5ffbde94fb4948367acf358c02f4
ARLikhan <arlikhan.nsucse@gmail.com>
2020-07-19 23:32:09 +0600

    reactjs website

---
+++ src/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import web from "../src/images/about.svg"
+import Common from './Common'
+
+const About = () => {
+    return( 
+    <> 
+    <Common 
+    name="Welcome to About Page"
+     imgsrc={web} 
+     visit="/contact"
+      btname="Contact Now" 
+      />
+
+    </>
+    );
+};
+
+export default About;
